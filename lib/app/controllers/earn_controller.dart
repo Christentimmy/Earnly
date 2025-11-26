2d25f0c256910326fce314e9c185776a43121c3d
Christentimmy <timmychris09@gmail.com>
2025-11-27 02:35:40 +0300

    earn controller

--- lib/app/controllers/earn_controller.dart
+++ lib/app/controllers/earn_controller.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:earnly/app/controllers/storage_controller.dart';
 import 'package:earnly/app/controllers/user_controller.dart';
 import 'package:earnly/app/data/services/earn_service.dart';
-import 'package:earnly/app/widgets/snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,13 +15,13 @@ class EarnController extends GetxController {
     getWheelSpinRewards();
   }
 
-  Future<void> wheelSpin() async {
+  Future<void> claimWheelSpin({required int reward}) async {
     try {
       final storageController = Get.find<StorageController>();
       final token = await storageController.getToken();
       if (token == null) return;
 
-      final response = await earnService.wheelSpin(token: token);
+      final response = await earnService.claimWheelSpin(token: token, reward: reward);
       if (response == null) return;
 
       final decoded = json.decode(response.body);
@@ -33,7 +32,6 @@ class EarnController extends GetxController {
         return;
       }
       await Get.find<UserController>().getUserDetails();
-      CustomSnackbar.showSuccessToast(message);
     } catch (e) {
       debugPrint(e.toString());
     }
--- lib/app/data/services/earn_service.dart
+++ lib/app/data/services/earn_service.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:earnly/app/utils/base_url.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class EarnService {
-  Future<http.Response?> wheelSpin({required String token}) async {
+  
+  Future<http.Response?> claimWheelSpin({
+    required String token,
+    required int reward,
+  }) async {
     try {
       final response = await http.post(
         Uri.parse('$baseUrl/earn/wheel-spin'),
@@ -11,6 +17,7 @@ class EarnService {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $token',
         },
+        body: jsonEncode({'reward': reward}),
       );
       return response;
     } catch (e) {
@@ -18,7 +25,7 @@ class EarnService {
     }
     return null;
   }
- 
+
   Future<http.Response?> getWheelSpinRewards({required String token}) async {
     try {
       final response = await http.get(
