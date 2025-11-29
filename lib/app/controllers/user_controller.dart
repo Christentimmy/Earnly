c782a5c487ce42e6bc68e7ef1db506f4882c8b1f
Christentimmy <timmychris09@gmail.com>
2025-11-29 21:38:12 +0300

    withdraw request

--- lib/app/controllers/user_controller.dart
+++ lib/app/controllers/user_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:earnly/app/controllers/storage_controller.dart';
 import 'package:earnly/app/data/models/user_model.dart';
 import 'package:earnly/app/data/services/user_service.dart';
+import 'package:earnly/app/widgets/snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -34,4 +35,38 @@ class UserController extends GetxController {
       debugPrint(e.toString());
     }
   }
+
+  Future<void> createWithdrawalRequest({
+    required String network,
+    required String address,
+    required String amount,
+  }) async {
+    try {
+      final storageController = Get.find<StorageController>();
+      final token = await storageController.getToken();
+      if (token == null) return;
+
+      final response = await userService.createWithdrawalRequest(
+        token: token,
+        network: network,
+        address: address,
+        amount: amount,
+      );
+      if (response == null) return;
+
+      final decoded = json.decode(response.body);
+      final message = decoded["message"] ?? "";
+
+      if (response.statusCode != 201) {
+        debugPrint(message);
+        CustomSnackbar.showErrorToast(message);
+        return;
+      }
+      CustomSnackbar.showSuccessToast(message);
+      Get.back();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
 }
