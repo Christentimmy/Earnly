8b6b7dc1c7d78b630c15d4f21200fa7ab1b5ba53
Christentimmy <timmychris09@gmail.com>
2025-12-05 19:55:04 +0300

    terms and condition style

--- lib/app/modules/settings/views/term_and_condition.dart
+++ lib/app/modules/settings/views/term_and_condition.dart
@@ -22,7 +22,8 @@ class TermAndCondition extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Text(""" 
+          child: Text(
+            """ 
       
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id ligula vulputate, fermentum leo vitae, vestibulum tortor. Etiam aliquet urna sed nibh ullamcorper condimentum. Aenean eu dui sit amet nunc consectetur pellentesque nec et tortor. Sed sit amet tristique est. Nulla urna sem, lobortis non condimentum non, porta a lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed convallis semper euismod. Morbi sodales dui nisl, ut iaculis mauris consequat eget. Maecenas aliquet pharetra ex id dapibus. Cras condimentum elit at libero rutrum, pharetra fermentum dui mollis. Fusce hendrerit lorem nulla, non facilisis velit faucibus vel. Morbi feugiat mollis tincidunt. Nam eleifend sagittis eros, sed gravida lorem tincidunt vitae. Morbi eleifend nec massa et aliquet.
 
@@ -33,7 +34,13 @@ Curabitur a accumsan ligula. Phasellus aliquet ipsum erat, quis mollis ex finibu
 Vestibulum tincidunt metus massa, vel scelerisque augue consectetur non. Aliquam id diam malesuada, ornare diam ut, molestie dolor. Nunc egestas, massa vel luctus commodo, magna magna accumsan sem, ultricies pulvinar ex ipsum ut ex. Vivamus suscipit, turpis vitae ornare auctor, elit libero porta orci, rutrum convallis magna urna sit amet purus. Morbi lobortis volutpat risus vel commodo. Nullam at nulla id nisi vulputate lobortis. Praesent et nibh vel leo consectetur volutpat. Proin suscipit purus ut felis sodales pharetra. Nullam maximus sed mi eget vehicula.
 
 Vivamus vitae facilisis massa. Sed lobortis pellentesque neque ac euismod. Duis venenatis ligula eget ante consectetur facilisis. Vivamus sit amet nisi nec arcu consequat ullamcorper. Proin vitae egestas elit. Fusce sit amet hendrerit velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula vulputate fringilla. Proin nec arcu facilisis, suscipit lectus ac, sodales ex. Morbi nec pulvinar sapien. Praesent non felis laoreet, ultrices ipsum et, fermentum urna. Suspendisse vitae viverra urna. Nullam sed sem commodo, rutrum lectus nec, consectetur felis.
-      """),
+      """,
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
       ),
     );
